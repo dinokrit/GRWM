ae2ca20d18f6580cc14652c350a55c50bd553026
DESKTOP-9GHIE3F\student <test>
2025-06-15 11:23:28 +0700

    babanana

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+# 👉 Code for Stage 7 goes here
 class Student:
-    def __init__(self, first_name, last_name, age, courses):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self, name, age, courses, pocket_money, location, salary):
+        self.name = name
         self.age = age
         self.courses = courses
+        self.money = pocket_money
+        self.location = location
+        self.salary = salary
 
-    def get_full_name(self):
-        return f"{self.first_name} {self.last_name}"
+#Function => Method
 
-    def display_info(self):
-        print("Name:", self.get_full_name())
-        print("Age:", self.age)
-        print("Courses:", ", ".join(self.courses))
-        print("-" * 20)
+def display_student_info(student):
+    print("Name:", student.name)
+    print("Age:", student.age)
+    print("Courses:", ", ".join(student.courses))
+    print("Pocket money:", student.money)
+    print("Location:", student.location)
+    print("Your salary is", student.salary)
+    print("-" * 20)
 
-student1 = Student("Alice", "Johnson", 20, ["Math", "English"])
-student2 = Student("Bob", "Smith", 22, ["Physics", "History"])
+student1 = Student("Alice", 20, ["Math", "English"], 120, "Wonder Land", 1024)
+student2 = Student("Bob", 22, ["Physics", "History"], 300, " Siren Cty", 5000000)
+student3 = Student("Nara", 4, ["Math", "Physics"], 1000000, "Frozen island", 20)
 
-student1.display_info()
-student2.display_info()
+display_student_info(student1)
+display_student_info(student2)
+display_student_info(student3)
+# Feel free to modify or expand it
